18d82f3a328658899d27eab3ee6bd9a5e67e3233
wuyangfan <1102042793@qq.com>
2022-04-18 21:28:29 +0800

    fix:格式化

--- challenges/medium-promise-all/template.ts
+++ challenges/medium-promise-all/template.ts
@@ -1,2 +1,6 @@
-declare function PromiseAll<T extends any[]>(values: readonly [...T]):
-  Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>;
+type Item<T> = { 
+  [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] 
+};
+declare function PromiseAll<T extends any[]>(
+    values: readonly [...T]
+): Promise<Item<T>>;
